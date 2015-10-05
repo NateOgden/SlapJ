289ff69dabfcdb63efadc822867078075bdcf1b8
Nate Ogden <Nathanogden@mail.weber.edu>
2015-10-04 22:57:58 -0600

    Rebuilding Project

---
+++ desktop/src/com/cs3750/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cs3750.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cs3750.Slapjack;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Slapjack(), config);
+	}
+}
