0c9f6e00867a2a9bb59430bdd18a106ff444e8cf
Diana <dianango@mail.weber.edu>
2015-10-06 17:46:44 -0600

    added a button, a start stage, and an end stage

--- core/src/com/cs3750/Slapjack.java
+++ core/src/com/cs3750/Slapjack.java
@@ -1,6 +1,7 @@
 package com.cs3750;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import com.badlogic.gdx.ApplicationAdapter;
@@ -27,9 +28,18 @@ public class Slapjack extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private Texture background;
 	private Texture cardSpriteSheet;
-	private Stage stage;
 	private Deck deck;
 	
+	private Stage startStage;
+	private Stage stage;
+	private Stage endStage;
+	
+	private Skin skin;
+	private BitmapFont bitmapfont;
+	private TextButtonStyle textButtonStyle;
+	
+	private HashMap<String, Runnable> buttonMap = new HashMap<String, Runnable>();
+	private TextButton playCardButton;
 	
 	//Don't delete create or render
 	@Override
@@ -38,8 +48,37 @@ public class Slapjack extends ApplicationAdapter {
 		background = new Texture("rustic_background.jpg");
 		cardSpriteSheet = new Texture(Gdx.files.internal("sprite_deck.png"));
 		deck = new Deck(cardSpriteSheet);
+		
+		startStage = new Stage();
 		stage = new Stage();
+		endStage = new Stage();
+		
+		//button style
+		bitmapfont = new BitmapFont();
+		skin = new Skin();
+		skin.add("default", bitmapfont);
+		skin.add("buttonUp", new Texture(Gdx.files.internal("button.png")));
+		skin.add("buttonDown", new Texture(Gdx.files.internal("buttonDown.png")));
+		
+		textButtonStyle = new TextButtonStyle();
+		textButtonStyle.up = skin.newDrawable("buttonUp");
+		textButtonStyle.down = skin.newDrawable("buttonDown");
+		textButtonStyle.font = skin.getFont("default");
+		
+		//create buttons
+		playCardButton = getButton("Play Card", (Gdx.graphics.getWidth())/2, 75, "playCardButton", textButtonStyle);
+		
+		//add buttons to map
+		buttonMap.put("playCardButton", new Runnable(){
+			public void run() {
+				//method called when the button is clicked
+			}
+		});
+		
+		//add actors to stage
+		stage.addActor(playCardButton);
 	}
+	
 	//Don't delete create or render
 	@Override
 	public void render () {
@@ -50,5 +89,19 @@ public class Slapjack extends ApplicationAdapter {
 		batch.end();
 		Gdx.input.setInputProcessor(stage); 
 		stage.draw();
+	}
+	
+	//this method is used to create a button
+	private TextButton getButton(String buttonText, int xPosition,
+			int yPosition, final String id, TextButtonStyle textButtonStyle) {
+		TextButton button = new TextButton(buttonText, textButtonStyle);
+		button.setPosition(xPosition, yPosition);
+		//this uses the buttonMap to get the correct method to run once a button is clicked
+		button.addListener(new ClickListener() {
+			public void clicked(InputEvent event, float x, float y) {
+				buttonMap.get(id).run();
+			}
+		});
+		return button;
 	}
 }
